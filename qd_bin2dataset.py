8a8d8eb20ba4d40f424867c55d0f9ed4fd13963c
Иван Лянгасов <lyangasovid@MacBook-Pro-Ivan.local>
2021-11-17 16:32:31 +0300

    add script for create dataset from quickdrow .bin

---
+++ qd_bin2dataset.py
@@ -0,0 +1,107 @@
+import struct
+from struct import unpack
+import numpy as np
+import cairocffi as cairo
+from PIL import Image 
+import os
+from tqdm import tqdm
+
+
+def unpack_drawing(file_handle):
+    key_id, = unpack('Q', file_handle.read(8))
+    country_code, = unpack('2s', file_handle.read(2))
+    recognized, = unpack('b', file_handle.read(1))
+    timestamp, = unpack('I', file_handle.read(4))
+    n_strokes, = unpack('H', file_handle.read(2))
+    image = []
+    for i in range(n_strokes):
+        n_points, = unpack('H', file_handle.read(2))
+        fmt = str(n_points) + 'B'
+        x = unpack(fmt, file_handle.read(n_points))
+        y = unpack(fmt, file_handle.read(n_points))
+        image.append((x, y))
+
+    return {
+        'key_id': key_id,
+        'country_code': country_code,
+        'recognized': recognized,
+        'timestamp': timestamp,
+        'image': image
+    }
+
+
+def unpack_drawings(filename):
+    with open(filename, 'rb') as f:
+        while True:
+            try:
+                yield unpack_drawing(f)
+            except struct.error:
+                break
+
+def covert_L_to_RGB(image_arr):
+    arr = np.zeros((256,256,3))
+    arr[:,:,0] = image_arr
+    arr[:,:,1] = image_arr
+    arr[:,:,2] = image_arr
+    arr = arr.astype(np.uint8)
+    return arr
+
+def vector_to_raster(vector_images, side=28, line_diameter=16, padding=16, bg_color=(0,0,0), fg_color=(1,1,1)):
+    """
+    padding and line_diameter are relative to the original 256x256 image.
+    """
+    
+    original_side = 256.
+    
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, side, side)
+    ctx = cairo.Context(surface)
+    ctx.set_antialias(cairo.ANTIALIAS_BEST)
+    ctx.set_line_cap(cairo.LINE_CAP_ROUND)
+    ctx.set_line_join(cairo.LINE_JOIN_ROUND)
+    ctx.set_line_width(line_diameter)
+
+    # scale to match the new size
+    # add padding at the edges for the line_diameter
+    # and add additional padding to account for antialiasing
+    total_padding = padding * 2. + line_diameter
+    new_scale = float(side) / float(original_side + total_padding)
+    ctx.scale(new_scale, new_scale)
+    ctx.translate(total_padding / 2., total_padding / 2.)
+
+    raster_images = []
+    for vector_image in vector_images:
+        # clear background
+        ctx.set_source_rgb(*bg_color)
+        ctx.paint()
+        
+        bbox = np.hstack(vector_image).max(axis=1)
+        offset = ((original_side, original_side) - bbox) / 2.
+        offset = offset.reshape(-1,1)
+        centered = [stroke + offset for stroke in vector_image]
+
+        # draw strokes, this is the most cpu-intensive part
+        ctx.set_source_rgb(*fg_color)        
+        for xv, yv in centered:
+            ctx.move_to(xv[0], yv[0])
+            for x, y in zip(xv, yv):
+                ctx.line_to(x, y)
+            ctx.stroke()
+
+        data = surface.get_data()
+        raster_image = np.copy(np.asarray(data)[::4])
+        raster_images.append(raster_image)
+    
+    images = [covert_L_to_RGB(im) for im in (255 - np.array(raster_images).reshape((-1,side,side)))]
+    return images
+
+
+def bin2dataset(bin_path, dataset_path, image_size=256, limit=-1):
+    drownings = unpack_drawings(bin_path)
+    for draw_id, drawing in tqdm(enumerate(drownings)):
+        if (limit > 0) and (limit < draw_id):
+            break
+        rastr_image = vector_to_raster([drawing['image']], side=image_size)[0]
+        image = Image.fromarray(rastr_image)
+
+        image_name = bin_path.split('/')[-1].split('.')[0] + f"{draw_id}.png"
+        image.save(os.path.join(dataset_path, image_name))
